9d7ba375ce6fa760924522b478ccefc9254a59b1
Ahmed Essam <ahmedessamfahim2005@gmail.com>
2026-04-09 23:18:44 +0200

    uploaded missing drivers

---
+++ ES_Course_Labs-drivers_task/APP/main.c
@@ -0,0 +1,25 @@
+#include "../SERVICES/STD_TYPES.h"
+#include "app_tests.h"
+
+void main(void) {
+
+
+    /* --- RUN ACTIVE TEST --- */
+
+    // APP_vidGpioTest();
+    // APP_vidExtiTest();
+    // APP_vidPwmTest();
+    // APP_vidAdcTest();
+    // APP_vidUartTest();
+    
+    // APP_vidTimer0Test();
+
+    // APP_vidSpiTest();
+    // APP_vidI2cTest();
+
+    while(1) {
+
+    }
+
+    return 0;
+}
